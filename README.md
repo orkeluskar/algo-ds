40a644f3ed8e642e26ecbfba62c06fbb1bd4f684
orkeluskar <ork216@nyu.edu>
2018-08-06 23:12:38 -0400

    Tree problems

---
+++ README.md
@@ -0,0 +1,33 @@
+## Introduction
+
+This is my javascript implementation of Algorithms & Data Structures
+
+
+
+## Getting Started
+
+Since, this is written in JavaScript, you'll need [Node](https://nodejs.org/) installed on your machine
+
+1. Clone the repo
+   ```bash
+   git clone https://github.com/orkeluskar/algo-ds.git
+   ```
+
+2. Change directory
+   ```bash
+   cd algo-ds
+   ```
+
+3. Test the code?
+   ```bash
+   npm test
+   ```
+
+
+
+## Project Structure
+
+- `/lib` - Contains all the necessary Data Structures like [Linked Lists](./lib/ListNode.js), [TreeNode](./lib/TreeNode.js) etc.
+- `/test` - Contains all the necessary test cases
+- Rest of the directories are named according to the aggregation of algorithm problems
+  - Also, each file contains a commented block to explain the problem
---
+++ test/trees-and-graphs/ValidateBST.test.js
@@ -0,0 +1,93 @@
+const expect = require('chai').expect
+const Node = require('../../lib/TreeNode')
+const ValidateBST = require('../../trees-and-graphs/ValidateBST')
+const ValidateBSTObj = new ValidateBST()
+
+describe('ValidateBST', function(){
+    it('Should check if the tree is Valid', function(){
+
+        //1. Arrange
+        //        4
+        //     /     \
+        //    2       6
+        //   / \     / \
+        //  1   3   5   7  
+        let root = new Node(4),
+            n1 = new Node(2),
+            n2 = new Node(6),
+            n3 = new Node(1),
+            n4 = new Node(3),
+            n5 = new Node(5),
+            n6 = new Node(7)
+        root.right = n2
+        root.left = n1
+        n1.left = n3
+        n1.right = n4
+        n2.left = n5
+        n2.right = n6
+
+        // 2. ACT
+        const isValid = ValidateBSTObj.checkBST(root)
+        
+        // 3. ASSERT
+        expect(isValid).to.be.equal(true);
+    })
+
+    it('Should check if the tree is Valid', function(){
+
+        //1. Arrange
+        //        4
+        //     /     \
+        //    2       6
+        //   / \     / \
+        //  1   9   5   7
+        let root = new Node(4),
+            n1 = new Node(2),
+            n2 = new Node(6),
+            n3 = new Node(1),
+            n4 = new Node(9),
+            n5 = new Node(5),
+            n6 = new Node(7)
+        root.right = n2
+        root.left = n1
+        n1.left = n3
+        n1.right = n4
+        n2.left = n5
+        n2.right = n6
+
+        // 2. ACT
+        const isValid = ValidateBSTObj.checkBST(root)
+        
+        // 3. ASSERT
+        expect(isValid).to.be.equal(false);
+    })
+
+    it('Should check if the tree is Valid', function(){
+
+        //1. Arrange
+        //        4
+        //     /     \
+        //    5       6
+        //   / \     / \
+        //  1   3   5   7
+        let root = new Node(4),
+            n1 = new Node(5),
+            n2 = new Node(6),
+            n3 = new Node(1),
+            n4 = new Node(9),
+            n5 = new Node(5),
+            n6 = new Node(7)
+        root.right = n2
+        root.left = n1
+        n1.left = n3
+        n1.right = n4
+        n2.left = n5
+        n2.right = n6
+
+        // 2. ACT
+        const isValid = ValidateBSTObj.checkBST(root)
+        
+        // 3. ASSERT
+        expect(isValid).to.be.equal(false);
+    })
+})
