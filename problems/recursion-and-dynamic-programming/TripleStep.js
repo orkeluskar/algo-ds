1da0ef3b9c0632111be55b469e12c93a51b4237b
orkeluskar <ork216@nyu.edu>
2018-08-11 11:29:38 -0400

    Triple Step in Recursion & DP

---
+++ problems/recursion-and-dynamic-programming/TripleStep.js
@@ -0,0 +1,57 @@
+/* 
+A child is running up a staircase with n steps an can hop either 1 step, 2 steps, or 3 steps at a time. 
+Implement a method to count how many possible ways the child can run up the stairs.
+*/
+
+class TripleStepSlower{
+    /**
+     * @param {number} n
+     * @return {number} 
+     */
+    countWays(n){
+        /* Runtime is exponential ~ O(n^3) */
+        if (n < 0) {
+            return 0
+        }
+        else if (n === 0) {
+            return 1
+        }
+        else {
+            return this.countWays(n - 1) + this.countWays(n - 2) + this.countWays(n - 3)
+        }
+    }
+}
+
+class TripleStep{
+    /**
+     * @param {number} n
+     * @return {number} 
+     */
+    countWays(n){
+        let memo = Array(n + 1).fill(-1)
+        return this.countWaysDP(n, memo)
+    }
+
+    /**
+     * @param {number} n
+     * @param {number[]} memo
+     * @return {number} 
+     */
+    countWaysDP(n, memo){
+        if (n < 0){
+            return 0
+        }
+        else if (n === 0){
+            return 1
+        }
+        else if (memo[n] > -1){
+            return memo[n]
+        }
+        else {
+            memo[n] = this.countWaysDP(n - 1, memo) + this.countWaysDP(n - 2, memo) + this.countWaysDP(n - 3, memo)
+            return memo[n]
+        }
+    }
+}
+
+module.exports = TripleStep
---
+++ test/recursion-and-dynamic-programming/TripleStep.test.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect
+const TripleStep = require('../../problems/recursion-and-dynamic-programming/TripleStep')
+const TripleStepObj = new TripleStep()
+
+describe('Triple Step', function(){
+    it('Should return possible ways to rup up staris in 1, 2 or 3 steps', function(){
+
+        // 1. Arrange
+        const stairs = 3,
+            expectedResult = 4 /* [1, 1, 1], [1, 2], [2, 1], [3] */
+        
+        // 2. Act
+        const actualResult = TripleStepObj.countWays(stairs)
+
+        // 3. Assert
+        expect(expectedResult).to.be.equal(actualResult)
+    })
+
+    it('Should return possible ways to rup up staris in 1, 2 or 3 steps', function(){
+
+        // 1. Arrange
+        const stairs = 4,
+            expectedResult = 7 /* [1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2], [3, 1], [1, 3] */
+        
+        // 2. Act
+        const actualResult = TripleStepObj.countWays(stairs)
+
+        // 3. Assert
+        expect(expectedResult).to.be.equal(actualResult)
+    })
+})
